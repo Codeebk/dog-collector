2e3c4932f66a62b1d4743534e0610dbe0b932a2d
Codee King <codee.king@gmail.com>
2019-06-10 15:48:49 -0500

    first commit

---
+++ main_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def dogs_index(request):
+    return render(request, 'dogs/index.html', { 'dogs': dogs })
