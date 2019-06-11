b8ce927e47203a3e01eb1cd5119eadc342738cfc
Codee King <codee.king@gmail.com>
2019-06-11 14:45:41 -0500

    Create Update and Delete implemented

--- main_app/models.py
+++ main_app/models.py
@@ -1,9 +1,37 @@
 from django.db import models
+from django.urls import reverse
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
 
 # Create your models here.
-
 class Dog(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+
+class Feeding(models.Model):
+  date = models.DateField()
+  meal = models.CharField(
+    max_length=1,
+    # add the choices field option
+    choices=MEALS,
+    # set the default value for meal to be 'B
+    default=MEALS[0][0])
+
+  # Create a dog_id FK
+  dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+
+  def __str__(self):
+    # Nice method for obtaining the friendly value of a Field.choice
+    return f"{self.get_meal_display()} on {self.date}"
+
+# def __str__(self):
+#     return self.name
+
+def get_absolute_url(self):
+    return reverse('detail', kwargs={'dog_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
 
 
-# class Dog:
-#     def __init__(self, name, breed, description, age):
-#         self.name = name
-#         self.breed = breed
-#         self.description = description
-#         self.age = age
+class DogCreate(CreateView):
+  model = Dog
+  fields = '__all__'
 
-# dogs = [
-#     Dog('Lolo', 'tabby', 'foul little demon', 3),
-#     Dog('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-#     Dog('Raven', 'black tripod', '3 legged cat', 4)
-# ]
+class DogUpdate(UpdateView):
+  model = Dog
+  # Let's make it impossible to rename a dog :)
+  fields = ['breed', 'description', 'age']
+
+class DogDelete(DeleteView):
+  model = Dog
+  success_url = '/dogs/'
 
 # Create your views here.
 def home(request):
